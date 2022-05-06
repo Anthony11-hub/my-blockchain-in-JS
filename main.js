331ca0b56c3c7fb310fdbec098ff78eb25b02e95
Anthony11-hub <mollycruz966@gmail.com>
2022-05-06 13:56:29 +0300

    created my own coin to test the blockchain

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const sha256 = require('crypto-js/sha256');
+const SHA256 = require('crypto-js/sha256');
 
 class Block{
     constructor(index, Timestamp, data, previousHash = ''){
@@ -12,13 +12,41 @@ class Block{
        this.data = data;
        this.Timestamp = Timestamp;
        this.previousHash = previousHash;
-       this.hash = '';//contais the hash of our block
+       this.hash = this.calculateHash();//contains the hash of our block
 
     }
 
     calculateHash(){//will take properties of this block, run it through hash fn and return the hash
         //this identifies our block on the blockchain
         //sha256 is used
+        return SHA256(this.index + this.previousHash +this.Timestamp + JSON.stringify(this.data));
 
     }
 }
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];//initialize our chain as array containing our genesis block
+    }
+
+    createGenesisBlock(){
+        //index, date, data = > string, previousBlock(in this case zero since its the first) 
+        return new Block(0, "01/01/2022", "Genesis Block", "0");
+    }
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(newBlock){//receives a block
+        newBlock.previousHash = this.getLatestBlock().hash;// gets previous block and sets it to last block on our chain
+        newBlock.hash = newBlock.calculateHash();// after changing our block, we have to recalculate its hash
+        this.chain.push(newBlock);//pushes it to the chain
+        //in reality we cannot add new blocks so easily since there are so many checks in place
+    }
+}
+
+//testing it -- my coin is called tcoin
+let tcoin = new Blockchain();
+tcoin.addBlock(new Block(1, "07/05/2022", { amount: 4 }));
+tcoin.addBlock(new Block(2, "04/05/2022", { amount: 7 }));
+
+console.log(JSON.stringify(tcoin, null, 4)); 
